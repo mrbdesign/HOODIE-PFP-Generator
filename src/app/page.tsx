8e1a6076f1e906246013795601d3475b468ef436
mrbriandesign <128382133+mrbdesign@users.noreply.github.com>
2026-07-13 12:50:04 +0900

    Remove main min-h-screen to eliminate footer gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PFPOverlayGenerator from './components/PFPOverlayGenerator';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4" style={{ backgroundColor: '#0F0F0F' }}>
+    <main className="flex flex-col items-center p-4" style={{ backgroundColor: '#0F0F0F' }}>
       <PFPOverlayGenerator />
     </main>
   );
